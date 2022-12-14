1309cf9f75e921c47e3b3dd7e57863ac03e7229c
MhdIqbalPratama <cromwell.tumb@gmail.com>
2022-12-14 22:54:56 +0700

    submit task2

---
+++ Days 2/jawaban/task.py
@@ -0,0 +1,43 @@
+## DAY 2
+# LV1
+nama = "Mhd Iqbal Pratama"
+jurusan = "Computer Science"
+angkatan = "2020"
+umur = 20
+asal_daerah = "Medan"
+money_amount = 99999999999
+is_mahasiswa = True
+is_alien = False
+matkul = ["Aljabar Linier", "Metode Numerik", "Machine Learning"]
+x,y,z = matkul
+print(x)
+print(y)
+print(z)
+print()
+## LV2
+print(type(nama))
+print(type(is_alien))
+print(nama)
+print(is_alien)
+print(len(nama))
+print 
+num_one = 5
+num_two = 4
+total = num_one+num_two
+diff = num_one-num_two
+product = num_one*num_two
+division = num_one/num_two
+remainder = num_one%num_two
+exp = num_one**num_two
+floor_division = num_one//num_two
+##
+name = input("Masukkan Nama : ")
+age = input("Masukkan Umur : ")
+regional = input("Asal Daerah : ")
+###
+radius = 30
+variable_area_of_circle = 3.14*radius*radius
+variable_circum_of_circle = 2*3.14*radius
+radius =input("Masukkan radius lingkaran : ")
+######
+help('print')
