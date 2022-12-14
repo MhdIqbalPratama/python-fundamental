7c02c3e4278eb9412d0d7872052c1f5a640ade29
MhdIqbalPratama <cromwell.tumb@gmail.com>
2022-12-14 22:25:01 +0700

    Submit Answer

---
+++ Days 1/jawaban/lv1.py
@@ -0,0 +1,39 @@
+#LV 1 & LV2
+print(5+6)
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+print(5%6)
+print(5**6)
+print(5//6)
+print()
+
+#2
+Nama = "Mhd Iqbal Pratama"
+NIM = "105220042"
+AsalDaerah = "Medan"
+
+#3
+print()
+print(type(12))
+print(type(7.4))
+print(type(3.14))
+print(type(5-6j))
+print(type(['Budi','105220001','Jakarta']))
+print(type(Nama))
+print(type(NIM))
+print(type(AsalDaerah))
+print()
+### lV3
+print()
+thistuple =("tes", "coba", "tuple")
+print(type(thistuple))
+#Euclidian
+a = 2
+b = 3
+c = 10
+e = 8
+dataec = ((a - b)**2 + (c - e)**2)**0.5
+print("Euclidian is : ")
+print(dataec)
